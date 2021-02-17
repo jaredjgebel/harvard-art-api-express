a0109f4d3979b61b646338644257306f8b9e48da
Jared <jaredjgebel@gmail.com>
2021-02-17 14:34:37 -0500

    Button styles

--- client/src/components/art-object/ArtObject.js
+++ client/src/components/art-object/ArtObject.js
@@ -1,12 +1,17 @@
 import React from "react";
+import styles from "./ArtObject.module.css";
 
 const ArtObject = ({ object }) => (
   <>
-    <p>{object.primaryimageurl}</p>
+    <p>{object.title}</p>
+    <img
+      className={styles.image}
+      src={object.primaryimageurl}
+      alt={object.title}
+    />
     <p>{object.century}</p>
     <p>{object.classification}</p>
     <p>{object.creditline}</p>
-    <p>{object.title}</p>
     <p>{object.url}</p>
   </>
 );

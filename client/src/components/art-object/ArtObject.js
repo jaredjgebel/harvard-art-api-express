33a1646a9b55c1f221fae4419b2d4eca5a49d14f
Jared <jaredjgebel@gmail.com>
2021-02-18 15:26:24 -0500

    Reformat form

--- client/src/components/art-object/ArtObject.js
+++ client/src/components/art-object/ArtObject.js
@@ -37,9 +37,37 @@ const ArtObject = ({ object }) => {
           {infoVisible && (
             <div className={styles.modalContainer}>
               <div className={styles.modal}>
-                {object.title}
-                {object.century}
-                {object.classification}
+                <div className={styles.modalItem}>
+                  <p>Title</p>
+                  <span>{object.title}</span>
+                </div>
+
+                <div className={styles.modalItem}>
+                  <p>Century</p>
+                  <span>{object.century}</span>
+                </div>
+
+                <div className={styles.modalItem}>
+                  <p>Classification</p>
+                  <span>{object.classification}</span>
+                </div>
+
+                <div className={styles.modalItem}>
+                  <p>Culture</p>
+                  <span>{object.culture}</span>
+                </div>
+
+                <div className={styles.modalItem}>
+                  <p>Dated</p>
+                  <span>{object.dated}</span>
+                </div>
+
+                {object.technique && (
+                  <div className={styles.modalItem}>
+                    <p>Technique</p>
+                    <span>{object.technique}</span>
+                  </div>
+                )}
               </div>
             </div>
           )}

fcb616915a94a4814fb31f6433048ebf6e129745
Jared <jaredjgebel@gmail.com>
2021-02-18 11:43:50 -0500

    Set up basic modal

--- client/src/components/art-object/ArtObject.js
+++ client/src/components/art-object/ArtObject.js
@@ -1,29 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+
 import styles from "./ArtObject.module.css";
 
-const ArtObject = ({ object }) => (
-  <>
-    <a
-      className={styles.url}
-      href={object.url}
-      target="_blank"
-      rel="noreferrer"
-    >
-      <h2 className={styles.title}>{object.title}</h2>
-    </a>
-    <div className={styles.info}>
-      <div>{object.century}</div>
-      <div>{object.classification}</div>
-    </div>
-    <div className={styles.imageContainer}>
-      <img
-        className={styles.image}
-        src={object.primaryimageurl}
-        alt={object.title}
-      />
-    </div>
-    <p className={styles.credit}>{object.creditline}</p>
-  </>
-);
+const ArtObject = ({ object }) => {
+  const [infoVisible, setInfoVisible] = useState(false);
+
+  return (
+    <>
+      <div className={styles.artObjectContainer}>
+        <div className={styles.artObjectContents}>
+          <a
+            className={styles.url}
+            href={object.url}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <h2 className={styles.title}>{object.title}</h2>
+          </a>
+
+          <div className={styles.info}>
+            <div>{object.century}</div>
+            {infoVisible ? (
+              <button
+                onClick={() => {
+                  setInfoVisible(false);
+                }}
+              >
+                Back
+              </button>
+            ) : (
+              <button onClick={() => setInfoVisible(true)}>More Info</button>
+            )}
+            <div>{object.classification}</div>
+          </div>
+
+          {infoVisible && (
+            <div className={styles.modalContainer}>
+              <div className={styles.modal}>
+                {object.title}
+                {object.century}
+                {object.classification}
+              </div>
+            </div>
+          )}
+          <div className={styles.imageContainer}>
+            <img
+              className={styles.image}
+              src={object.primaryimageurl}
+              alt={object.title}
+            />
+          </div>
+          <p className={styles.credit}>{object.creditline}</p>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default ArtObject;

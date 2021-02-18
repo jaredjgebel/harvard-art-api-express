365c7bb42cc56578bbf12ed045298f9b85179ab9
Jared <jaredjgebel@gmail.com>
2021-02-18 15:26:13 -0500

    Reformat Header

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -4,7 +4,8 @@ import styles from "./Header.module.css";
 function Header() {
   return (
     <div className={styles.header}>
-      <h1>Harvard Art Museums API Search</h1>
+      <h1>Harvard Art Museums Search</h1>
+      <h2>All images are the property of Harvard Art Museums</h2>
     </div>
   );
 }
